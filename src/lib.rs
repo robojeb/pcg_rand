664ad57fa15769050ef53f25f1d0ccb444985385
Jeb Brooks <robojeb@gmail.com>
2015-10-22 15:17:15 -0400

    Created a binary to demonstrate some of what PCG can do. Implemented basic pcg32

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,52 @@
+extern crate rand;
+
+use rand::{Rng, Rand};
+
+use std::num::Wrapping;
+
+pub struct Pcg32Basic {
+    state : u64,
+    inc   : u64,
+}
+
+impl Pcg32Basic {
+    pub fn new_unseeded() -> Pcg32Basic {
+        Pcg32Basic{
+            state : 0,
+            inc : 0,
+        }
+    }
+}
+
+//Pcg32Basic is an rng
+impl Rng for Pcg32Basic {
+    #[inline]
+    fn next_u32(&mut self) -> u32 {
+        let oldstate = Wrapping(self.state);
+        //Update the state as an lcg
+        self.state = (oldstate * Wrapping(6364136223846793005u64) + Wrapping(self.inc | 1)).0;
+
+        //Prepare the permutation on the output
+        let xorshifted : u32 = (((oldstate >> 18usize) ^ oldstate) >> 27usize).0 as u32;
+        let rot : u32 = (oldstate >> 59usize).0 as u32;
+
+        //Produce the permuted output
+        (xorshifted >> rot) | (xorshifted << ((-(rot as i32)) & 31))
+    }
+}
+
+//Pcg32Basic can be randomly initialized with system entropy (or any other RNG)
+impl Rand for Pcg32Basic {
+    fn rand<R: Rng>(other: &mut R) -> Pcg32Basic {
+        Pcg32Basic{
+            state : other.gen(),
+            inc   : other.gen(),
+        }
+    }
+}
+
+
 #[test]
 fn it_works() {
+    let mut rng = Pcg32Basic::new_unseeded();
 }

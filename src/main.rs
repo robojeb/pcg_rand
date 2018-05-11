ab8903a378eaac6b9805c62896d4fc3c9c3cdde0
Jeb Brooks <robojeb@gmail.com>
2018-05-11 13:31:11 -0700

    Updated main to have all the examples back

--- src/main.rs
+++ src/main.rs
@@ -22,55 +22,50 @@ extern crate pcg_rand;
 extern crate rand;
 
 use rand::{RngCore, Rng, FromEntropy};
-use pcg_rand::{Pcg32Basic}; //Pcg32, Pcg64, Pcg32Unique, Pcg32L};
+use pcg_rand::{Pcg32Basic, Pcg32, Pcg64, Pcg32Unique, Pcg32L};
 
 
 #[cfg(not(test))]
 fn main() {
-    let mut rng = Pcg32Basic::from_entropy();
+    let mut rng = Pcg32Basic::new_unseeded();
 
     // print a bunch of random numbers
-    println!("Here is the generator seeded from system entropy: ");
+    println!("Here is the generator recovering from a (0,0) initialization: ");
     for _ in 0..10 {
         println!("{: ^25}|{: ^25}|{: ^25}", rng.gen::<u32>(), rng.gen::<u32>(), rng.gen::<u32>());
     }
 
-    let mut rng = Pcg32Basic::from_entropy();
-
-    let mut x = vec![0; 1024*1024];
-    rng.fill_bytes(x.as_mut_slice());
-
-    // let mut rng : Pcg32 = OsRng::new().unwrap().gen();
-    // println!("\nHere is the 32bit generator with random seed and increment: ");
-    // for _ in 0..10 {
-    //     println!("{: ^25}|{: ^25}|{: ^25}", rng.gen::<u32>(), rng.gen::<u32>(), rng.gen::<u32>());
-    // }
+    let mut rng : Pcg32 = Pcg32::from_entropy();
+    println!("\nHere is the 32bit generator with random seed and increment: ");
+    for _ in 0..10 {
+        println!("{: ^25}|{: ^25}|{: ^25}", rng.gen::<u32>(), rng.gen::<u32>(), rng.gen::<u32>());
+    }
     
-    // let mut rng : Pcg64 = OsRng::new().unwrap().gen();
-    // println!("\nHere is the 64bit generator with random seed and increment: ");
-    // for _ in 0..10 {
-    //     println!("{: ^25}|{: ^25}|{: ^25}", rng.gen::<u64>(), rng.gen::<u64>(), rng.gen::<u64>());
-    // }
+    let mut rng : Pcg64 = Pcg64::from_entropy();
+    println!("\nHere is the 64bit generator with random seed and increment: ");
+    for _ in 0..10 {
+        println!("{: ^25}|{: ^25}|{: ^25}", rng.gen::<u64>(), rng.gen::<u64>(), rng.gen::<u64>());
+    }
     
-    // let mut rng : Pcg32L = OsRng::new().unwrap().gen();
-    // println!("\nHere is the 32bit generator with 128 bits of internal state random seed and increment: ");
-    // for _ in 0..10 {
-    //     println!("{: ^25}|{: ^25}|{: ^25}", rng.gen::<u32>(), rng.gen::<u32>(), rng.gen::<u32>());
-    // }
+    let mut rng : Pcg32L = Pcg32L::from_entropy();
+    println!("\nHere is the 32bit generator with 128 bits of internal state random seed and increment: ");
+    for _ in 0..10 {
+        println!("{: ^25}|{: ^25}|{: ^25}", rng.gen::<u32>(), rng.gen::<u32>(), rng.gen::<u32>());
+    }
 
-    // // //Later we will do party tricks
-    // // //TODO: Implement the really big generators to get the party started
+    // //Later we will do party tricks
+    // //TODO: Implement the really big generators to get the party started
 
-    // println!("\nHere we show off what two unique stream generators with the same seed can do");
-    // println!("Example Code:
-    // let mut urng1 = Pcg32Unique::new_unseeded();
-    // let mut urng2 = Pcg32Unique::new_unseeded();
-    // ");
-    // let mut urng1 = Pcg32Unique::new_unseeded();
-    // let mut urng2 = Pcg32Unique::new_unseeded();
-    // println!("{: ^25}|{: ^25}", "Generator 1", "Generator2");
-    // for _ in 0..25 {
-    //     println!("{: ^25}|{: ^25}", urng1.gen::<u32>(), urng2.gen::<u32>());
-    // }
-    // println!("The RNGs use their location as a sequence so they diverge quickly");
+    println!("\nHere we show off what two unique stream generators with the same seed can do");
+    println!("Example Code:
+    let mut urng1 = Pcg32Unique::new_unseeded();
+    let mut urng2 = Pcg32Unique::new_unseeded();
+    ");
+    let mut urng1 = Pcg32Unique::new_unseeded();
+    let mut urng2 = Pcg32Unique::new_unseeded();
+    println!("{: ^25}|{: ^25}", "Generator 1", "Generator2");
+    for _ in 0..25 {
+        println!("{: ^25}|{: ^25}", urng1.gen::<u32>(), urng2.gen::<u32>());
+    }
+    println!("The RNGs use their location as a sequence so they diverge quickly");
 }
